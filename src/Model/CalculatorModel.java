9c34115fd80e309ad9dd4314bbc6cdb75c4fb5fb
Sami Cakiral <sami@MacBook-Air-de-Sami.local>
2023-10-13 14:28:37 +0200

    implémentation de add and sub

--- src/Model/CalculatorModel.java
+++ src/Model/CalculatorModel.java
@@ -5,5 +5,8 @@ public class CalculatorModel implements CalculatorModelInterface{
 	public int add(int nombre1, int nombre2) {
 		return nombre1 + nombre2;
 	}
+	public int sub(int nombre1, int nombre2) {
+		return nombre1 - nombre2;
+	}
 
 }
--- src/Model/CalculatorModelInterface.java
+++ src/Model/CalculatorModelInterface.java
@@ -1,5 +1,9 @@
 package Model;
 
 public interface CalculatorModelInterface {
+
+    public int add(int nombre1, int nombre2);
+    public int sub(int nombre1, int nombre2);
+
     
 }
