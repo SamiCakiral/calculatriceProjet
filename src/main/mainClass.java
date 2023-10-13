c95098c07bf5c0381deee6b55192065d323a6474
Sami Cakiral <sami@MacBook-Air-de-Sami.local>
2023-10-13 14:16:45 +0200

    début implémentation main class

--- src/main/mainClass.java
+++ src/main/mainClass.java
@@ -1,7 +1,15 @@
 package main;
 
+import Model.CalculatorModel;
+
 public class mainClass {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CalculatorModel model = new CalculatorModel();
+        // bufferreader for input
+        int nombre1 = 0;
+        int nombre2 = 0;
+
+        model.add(nombre1, nombre2);
+        // buffer for output
     }
 }
