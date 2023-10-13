a7eea96397a59c6a4509d39b548e66ba3b53b699
Sami Cakiral <sami@MacBook-Air-de-Sami.local>
2023-10-13 14:45:48 +0200

    Implémentation de l'interface

--- src/Model/CalculatorModel.java
+++ src/Model/CalculatorModel.java
@@ -2,11 +2,49 @@ package Model;
 
 public class CalculatorModel implements CalculatorModelInterface{
 
+	@Override
 	public int add(int nombre1, int nombre2) {
 		return nombre1 + nombre2;
 	}
+	@Override
 	public int sub(int nombre1, int nombre2) {
 		return nombre1 - nombre2;
 	}
+	@Override
+	public int multiply(int nombre1, int nombre2) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'multiply'");
+	}
+	@Override
+	public int divide(int nombre1, int nombre2) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'divide'");
+	}
+	@Override
+	public int opposite(int nombre1) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'opposite'");
+	}
+	@Override
+	public void push(String element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'push'");
+	}
+	@Override
+	public String pop() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'pop'");
+	}
+	@Override
+	public void drop() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'drop'");
+	}
+	@Override
+	public void swap() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'swap'");
+	}
+	
 
 }
--- src/Model/CalculatorModelInterface.java
+++ src/Model/CalculatorModelInterface.java
@@ -4,6 +4,14 @@ public interface CalculatorModelInterface {
 
     public int add(int nombre1, int nombre2);
     public int sub(int nombre1, int nombre2);
+    public int multiply(int nombre1, int nombre2);
+    public int divide(int nombre1, int nombre2);
+    public int opposite(int nombre1);
+    public void push(String element);
+    public String pop();
+    public void drop();
+    public void swap();
+
 
     
 }
